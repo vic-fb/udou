3b3bda842ca095a204a2201f9ce456fe88967a38
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-03 00:10:37 +0100

    refactor(app): Add style file for app

---
+++ src/application/App.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { Avatar as AntdAvatar } from 'antd';
+import { primaryColor } from '../config';
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 0 25px;
+  width: 100%;
+`;
+
+export const Avatar = styled(AntdAvatar)`
+  background-color: ${primaryColor};
+`;
